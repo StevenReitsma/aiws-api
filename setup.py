f86a8874387d719650b568a4df87081abf072ee5
Robbert van der Gugten <robbertvdg@gmail.com>
2019-12-02 16:13:03 +0100

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='rlclientbdr',
-     version='1.0',
+     version='1.1',
      scripts=[],
      author="Robbert van der Gugten",
      author_email="robbert.van.der.gugten@bigdatarepublic.nl",
